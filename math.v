4c15b08a8d2466b7ed8bc3659759e752f7025e73
Soundwave <PS-Soundwave@users.noreply.github.com>
2022-12-24 21:49:01 -0500

    Suppose Lukasiewicz' one stroke axiom and modus ponens for stroke

--- math.v
+++ math.v
@@ -2,3 +2,6 @@ Declare ML Module "ltac_plugin".
 Set Default Proof Mode "Classic".
 
 Axiom stroke : forall (p q : Prop), Prop.
+
+Axiom ax_mp : forall {p q r : Prop} (min : p) (maj : (stroke p (stroke q r))), r.
+Axiom ax_luk : forall (p q r s : Prop), (stroke (stroke p (stroke q r)) (stroke (stroke s (stroke s s)) (stroke (stroke s q) (stroke (stroke p s) (stroke p s))))).
