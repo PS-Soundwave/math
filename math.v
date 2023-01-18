1f83cab50fadc95769f56b8ee667dbd211c824ae
Soundwave <PS-Soundwave@users.noreply.github.com>
2023-01-18 11:31:49 -0500

    Replace inductive definition with axiom to call attention

--- math.v
+++ math.v
@@ -1,7 +1,7 @@
 Declare ML Module "ltac_plugin".
 Set Default Proof Mode "Classic".
 
-Inductive stroke (p q : Prop) : Prop :=.
+Axiom stroke : forall (p q : Prop), Prop.
 
 Axiom ax_mp : forall {p q r : Prop} (min : p) (maj : (stroke p (stroke q r))), r.
 Axiom ax_luk : forall (p q r s : Prop), (stroke (stroke p (stroke q r)) (stroke (stroke s (stroke s s)) (stroke (stroke s q) (stroke (stroke p s) (stroke p s))))).
